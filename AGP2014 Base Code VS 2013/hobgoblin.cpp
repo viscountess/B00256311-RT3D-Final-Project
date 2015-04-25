7674d703d00b9bfd163c0bede288692141d3dd33
viscountess <venerable_viscountess@hotmail.com>
2015-04-25 16:54:33 +0100

    Hobgoblin update
    Hobgoblin now is refactored and renders onscreen

--- AGP2014 Base Code VS 2013/hobgoblin.cpp
+++ AGP2014 Base Code VS 2013/hobgoblin.cpp
@@ -1,9 +1,11 @@
 #include "hobgoblin.h"
+#include <SDL_ttf.h>
+using namespace std;
 
 //constructor
 Hobgoblin::Hobgoblin()
 {
-
+	int currentAnim = 0;
 }
 
 //deconstructor
@@ -14,10 +16,83 @@ Hobgoblin::~Hobgoblin()
 
 void Hobgoblin::initialise()
 {
+	textures[1] = rt3d::loadBitmap("hobgoblin2.bmp");
+	meshObjects[1] = tmpModel.ReadMD2Model("tris.MD2");
+	md2VertCount = tmpModel.getVertDataCount();
 
+	material1 = {
+		{ 0.4f, 0.4f, 1.0f, 1.0f }, // ambient
+		{ 0.8f, 0.8f, 1.0f, 1.0f }, // diffuse
+		{ 0.8f, 0.8f, 0.8f, 1.0f }, // specular
+		1.0f  // shininess
+	};
+
+	shaderProgram = rt3d::initShaders("phong-tex.vert", "phong-tex.frag");
+	//rt3d::setLight(shaderProgram, light0);
+
+	pos.x = 1.0f;
+	pos.y = 1.2f;
+	pos.z = -5.0f;
 }
 
-void Hobgoblin::render()
+void Hobgoblin::render(std::stack<glm::mat4>& _Stack)
 {
+	GLfloat scale(1.0f);
 
+	// Animate the md2 model, and update the mesh with new vertex data
+	tmpModel.Animate(currentAnim, 0.1);
+	rt3d::updateMesh(meshObjects[1], RT3D_VERTEX, tmpModel.getAnimVerts(), tmpModel.getVertDataSize());
+
+	// draw the hobgoblin
+	glCullFace(GL_FRONT); // md2 faces are defined clockwise, so cull front face
+	glBindTexture(GL_TEXTURE_2D, textures[1]);
+	rt3d::materialStruct tmpMaterial = material1;
+	rt3d::setMaterial(shaderProgram, tmpMaterial);
+	_Stack.push(_Stack.top());
+	_Stack.top() = glm::translate(_Stack.top(), pos);
+	_Stack.top() = glm::rotate(_Stack.top(), 90.0f, glm::vec3(0.0f, -1.0f, 0.0f));
+	_Stack.top() = glm::rotate(_Stack.top(), 90.0f, glm::vec3(-1.0f, 0.0f, 0.0f));
+	
+	_Stack.top() = glm::scale(_Stack.top(), glm::vec3(scale*0.05, scale*0.05, scale*0.05));
+	rt3d::setUniformMatrix4fv(shaderProgram, "modelview", glm::value_ptr(_Stack.top()));
+	rt3d::drawMesh(meshObjects[1], md2VertCount, GL_TRIANGLES);
+	_Stack.pop();
+	glCullFace(GL_BACK);
+}
+
+void Hobgoblin::update(void)
+{
+	currentAnim = 0;
+
+	const Uint8 *keys = SDL_GetKeyboardState(NULL);
+
+	if (keys[SDL_SCANCODE_W]){
+		//eye = moveForward(eye, r, 0.1f);
+		pos.z += 0.1f;
+		currentAnim = 1;
+
+	}
+	if (keys[SDL_SCANCODE_S]) //eye = moveForward(eye, r, -0.1f);
+	{
+		pos.z -= 0.1f;
+		currentAnim = 1;
+	}
+	if (keys[SDL_SCANCODE_A]) //eye = moveRight(eye, r, -0.1f);
+	{
+		pos.x += 0.1f;
+		currentAnim = 1;
+	}
+	if (keys[SDL_SCANCODE_D]) //eye = moveRight(eye, r, 0.1f);
+	{
+		pos.x -= 0.1f;
+		currentAnim = 1;
+	}
+	if (keys[SDL_SCANCODE_Z]) {
+		if (--currentAnim < 0) currentAnim = 19;
+		cout << "Current animation: " << currentAnim << endl;
+	}
+	if (keys[SDL_SCANCODE_X]) {
+		if (++currentAnim >= 20) currentAnim = 0;
+		cout << "Current animation: " << currentAnim << endl;
+	}
 }
--- AGP2014 Base Code VS 2013/hobgoblin.h
+++ AGP2014 Base Code VS 2013/hobgoblin.h
@@ -1,6 +1,14 @@
 #ifndef HOBGOBLIN_INCLUDE
 #define HOBGOBLIN_INCLUDE
 
+#include "md2model.h"
+#include "rt3d.h"
+#include "rt3dObjLoader.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <stack>
+#include <SDL_ttf.h>
 
 class Hobgoblin
 {
@@ -10,10 +18,29 @@ public:
 	//deconstructor
 	~Hobgoblin();
 	void initialise();
-	void render();
-private:
+	void render(std::stack<glm::mat4>& _stack);
+	void update(void);
+	glm::vec3 getPos() { return pos; }
 
+private:
+	// md2 stuff
+	md2model tmpModel;
+	int currentAnim;
+
+	GLuint meshIndexCount = 0;
+	GLuint md2VertCount = 0;
+	GLuint meshObjects[2];
+
+	GLuint textures[2];
+
+	GLuint shaderProgram;
+
+	rt3d::materialStruct material1;
+
+	glm::vec3 pos;
+	//GLfloat Zpos;
 };
+
 
 
 #endif  //HOBGOBLIN_INCLUDE
