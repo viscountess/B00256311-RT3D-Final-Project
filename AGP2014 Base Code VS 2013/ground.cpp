cc9ba088422d9d919f90a6d5a82c4701bb9ad339
viscountess <venerable_viscountess@hotmail.com>
2015-04-26 15:52:40 +0100

    Setting up refactoring base for ground

---
+++ AGP2014 Base Code VS 2013/ground.cpp
@@ -0,0 +1,23 @@
+#include "ground.h"
+
+//constructor
+Ground::Ground()
+{
+
+}
+
+//deconstructor
+Ground::~Ground()
+{
+
+}
+
+void Ground::initialise()
+{
+
+}
+
+void Ground::render()
+{
+
+}
---
+++ AGP2014 Base Code VS 2013/ground.h
@@ -0,0 +1,21 @@
+#ifndef GROUND_INCLUDE
+#define GROUND_INCLUDE
+
+
+class Ground
+{
+public:
+	//constructor
+	Ground();
+
+	//deconstructor
+	~Ground();
+
+	void initialise();
+
+	void render();
+
+private:
+
+};
+#endif //GROUND_INCLUDE
